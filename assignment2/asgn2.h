bfe90b4104787f9e41a2ffa70a960f71e7339dc1
ntaylor50 <150801152+ntaylor50@users.noreply.github.com>
2025-02-04 04:18:31 +0000

    initial

---
+++ assignment2/asgn2.h
@@ -0,0 +1,65 @@
+#indef __asgn2__
+#define __asgn2__
+#include <stdio.h> 
+
+
+int calculateDiscount(price, discount) {
+    int price = ;
+    int discount = ;
+    // discount = (discount/price * 100)
+    
+    return 0; 
+}
+
+
+
+
+int greaterThanIdex (int arr[], int size){
+
+int arrayOfNumbers[3] = {10, 20, 30};
+int arrayOfNumbers[5]= {1, 2, 44, 29, 309};
+int arrayOfNumbers[0]=  {0}
+
+
+}
+
+int sumofDigits(int n) {
+   int sum = 0;
+   while (n > 0) {
+    sum += n % 10;
+    n/= 10;
+   }
+    return 0;
+}
+
+
+
+
+int grader(int attendance, float avg_exams, float avg_hw,);
+    int attendance;
+    float avg_exams;
+    float avg_hw;
+    
+
+
+
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+#endif
+
