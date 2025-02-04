d17123d7851ee37533f3ebfd471f8832b20e30e8
ntaylor50 <150801152+ntaylor50@users.noreply.github.com>
2025-02-04 04:17:47 +0000

    initial

---
+++ assignment2/asgn2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include "asgn2.h"
+
+int main() 
+{
+    int price; 
+    int discount; 
+    int result = price/discount * 100
+
+    printf("Enter price amount": )
+    printf ("Enter discount amount": )
+    printf("Final price is: price %d\n");
+    
+    return 0;
+
+
+    int arrayOfNumbers[] = {}
+    int arr[5] = {10, 20, 1, 20, 3 };
+
+    int myArr[] =
+    int myArr2[]= 
+    int myArr3[]=
+    int arraySize = 
+  
+   for(int i = 0; i < arraySize; i++){
+    print("The number at index %d is %d\n", i, myArr[i]);
+    printf(%d, n);
+    
+    return 0; 
+
+
+   int n;
+    printf ("Enter integer n:  ");
+    printf ("The sum of %d is: ");
+
+    return 0; 
+
+ 
+ int attendance;
+ float avg_exams;
+ float avg_hw;
+
+ printf("Enter student's attendance, average exams, and average hw assignmnets: ");
+ scanf(%d %f %f, attendance, avg_exams, avg_hw);
+
+    if  (attendance <= 22)
+        printf("FAIL\n");
+    }
+    else if (avg_exams <= 75) {
+        printf("FAIL\n");
+    }
+    else if (avg_hw <= 75) {
+        printf("FAIL\n");
+    }
+    else if (avg_exams > 80) {
+        printf("PASS\n");
+    }
+    else if (avg_hw > 80) {
+        printf("PASS\n");
+    }
+    return 0; 
+    
+   }
